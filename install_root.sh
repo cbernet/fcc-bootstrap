6d9d33a9d986be4e44368c51dbb9e9a83738e66a
Colin <colin.bernet@cern.ch>
2016-01-06 11:47:12 +0100

    simple install script for ROOT

---
+++ install_root.sh
@@ -0,0 +1,16 @@
+base=$PWD
+root_build_dir=$base/Softare/root
+root_install_dir=$base/Install
+root_tgz=root_v5.34.34.source.tar.gz
+threads=4
+
+mkdir -p $root_build_dir
+cd $root_build_dir
+curl -O https://root.cern.ch/download/$root_tgz
+tar -zxvf $root_tgz
+cd root
+mkdir -p $root_install_dir
+./configure --prefix=$root_install_dir
+make -j $threads install
+
+source $root_install_dir/bin/thisroot.sh
